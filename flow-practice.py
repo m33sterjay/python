dd97f2297830b17c36dbce6189aa3984eaa6c424
mr_jay <m33sterjay@gmail.com>
2022-06-13 23:21:10 -0500

    practicing flow control statements such as if, else, & elif statements

---
+++ flow-practice.py
@@ -0,0 +1,10 @@
+name = input('Whats your name? ')
+if name == 'Jared':
+    print('Hi Jared')
+elif name != 'Jared':
+    print('eww, who are you?')
+age = int(input('How old are you? '))
+if age <= 29:
+    print('What a nice young man you are')
+elif age > 29:
+    print('You old shit')
